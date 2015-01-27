2809a70c50a8647fd08148c6a5196b467256ac28
Gabriel <gabeira@gmail.com>
2015-01-26 20:55:20 -0400

    fragment

---
+++ app/src/main/java/br/com/caelum/cadastro/ListaProvasFragment.java
@@ -0,0 +1,39 @@
+package br.com.caelum.cadastro;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class ListaProvasFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View layoutProvas = inflater.inflate(R.layout.fragment_lista_provas,container,false);
+        return layoutProvas;
+    }
+
+//    @Override
+//    public void onAttach(Activity activity) {
+//        super.onAttach(activity);
+//        try {
+//            mListener = (OnFragmentInteractionListener) activity;
+//        } catch (ClassCastException e) {
+//            throw new ClassCastException(activity.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
+
+}
---
+++ app/src/main/java/br/com/caelum/cadastro/Prova.java
@@ -0,0 +1,22 @@
+package br.com.caelum.cadastro;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by aluno2 on 26/01/15.
+ */
+public class Prova implements Serializable {
+
+    private String data;
+    private String data;
+    private String data;
+    private List<String> topicos = new ArrayList<String>;
+
+
+
+
+
+
+}
